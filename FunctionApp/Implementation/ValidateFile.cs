7956f770564e946565e3e2130290c32263771b8f
NisheethRawat <40361445+NisheethRawat@users.noreply.github.com>
2021-02-13 08:58:35 +0000

    main

--- FunctionApp/Implementation/ValidateFile.cs
+++ FunctionApp/Implementation/ValidateFile.cs
@@ -7,26 +7,31 @@ namespace FunctionApp
 {
     public class ValidateFile : IValidateFile
     {
-        public void ValidateFileContent(ref string[] fileLines) {
-                fileLines = fileLines.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+        public void ValidateFileContent(ref string[] fileLines)
+        {
+            fileLines = fileLines.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            this.CheckOperatorAndOperand(ref fileLines);
+            this.CheckForApply(ref fileLines);
+        }
 
-                #region Check operator and operand list
-                decimal parsedId;
-                List<string> operatorsList = new List<string>() { "add", "divide", "multiply", "subtract", "apply" };
-                foreach (var line in fileLines)
-                {
-                    if (!operatorsList.Contains(line.Split(" ")[0].ToLower()))
-                        throw new Exception("Invalid File Format");
-
-                    if (!decimal.TryParse(line.Split(" ")[1], out parsedId))
-                        throw new Exception("Invalid File Format");
-                }
-                #endregion
-
-                #region Check apply is present
-                if (fileLines.Where(x => x.ToLower().Contains("apply")).ToList().Count <= 0)
+        private void CheckOperatorAndOperand(ref string[] fileLines)
+        {
+            decimal parsedId;
+            List<string> operatorsList = new List<string>() { "add", "divide", "multiply", "subtract", "apply" };
+            foreach (var line in fileLines)
+            {
+                if (!operatorsList.Contains(line.Split(" ")[0].ToLower()))
                     throw new Exception("Invalid File Format");
-                #endregion
+
+                if (!decimal.TryParse(line.Split(" ")[1], out parsedId))
+                    throw new Exception("Invalid File Format");
             }
         }
+
+        private void CheckForApply(ref string[] fileLines)
+        {
+            if (fileLines.Where(x => x.ToLower().Contains("apply")).ToList().Count <= 0)
+                throw new Exception("Invalid File Format");
+        }
+    }
 }
